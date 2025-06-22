0896989431f699bfa18e7ecbda5b9e6f219b5144
Vivek <bunkarvivek75@gmail.com>
2025-06-22 12:21:02 +0530

    Frontend part done

--- frontend/src/pages/Contact.jsx
+++ frontend/src/pages/Contact.jsx
@@ -8,6 +8,7 @@ function Contact() {
       <div className='text-center text-2xl pt-10 border-t'>
         <Title text1={'CONTACT'} text2={'US'}/>
       </div>
+      
       <div className='my-10 flex flex-col justify-center md:flex-row gap-10 mb-28'>
         <img className='w-full md:max-w-[480px]' src={assets.contact_img} alt="" />
         <div className='flex flex-col justify-center items-start gap-6'>
